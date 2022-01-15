5f24f5995ffc9afb6a4199ff994bab1e948535c4
Glitch (glitch-hello-website) <none>
2022-01-15 17:52:41 +0000

    👯🌈 Checkpoint ./.glitch-assets:31616692/745 ./script.js:31616692/304

--- script.js
+++ script.js
@@ -178,7 +178,8 @@ function setupGLTF() {
   scene = new THREE.Scene();
 
   loader.load(
-    "https://cdn.glitch.com/0aa4cfe1-11c0-401b-8a81-9c5907f3dd8b%2Fbrushmantest23.gltf?v=1630872801734",
+    // "https://cdn.glitch.com/0aa4cfe1-11c0-401b-8a81-9c5907f3dd8b%2Fbrushmantest23.gltf?v=1630872801734",
+    "https://cdn.glitch.global/0aa4cfe1-11c0-401b-8a81-9c5907f3dd8b/mixamoTrial.gltf?v=1642268879609",
 
     (gltfData) => {
       // called when the resource is loaded
